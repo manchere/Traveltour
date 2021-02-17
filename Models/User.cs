3a700d5bec2b1763a2aafb5a1b1aab4cc801efc5
Manu Cheremeh <manucheremeh1995@gmail.com>
2021-02-17 23:59:43 +0000

    User model created

---
+++ Models/User.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Traveltour.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string Firstname { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Othername { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Lastname { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime RegisterDate { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Gender { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime DOB { get; set; }
+    }
+}
